96288f77652d4bb9583c84cc9a1572712d1038bf
nicolasrhone <nicolas.rhone@gmail.com>
2016-06-04 10:58:45 +0200

    publie

---
+++ README.md
@@ -0,0 +1,16 @@
+Survey
+======
+
+Rubedo extension for survey feature
+
+Rubedo 3.x version
+This application is used to conduct online surveys. Stages and questions can be easly created and manged through drag and drop. The "Surveys" application allows:
+
+- to add, edit, delete, and duplicate a survey
+- to add a survey to favorites
+- to manage the settings and unique answer conditions of a survey and put it online
+- to create a survey using the tool palette of formatting (Wysiwyg, image, section, page, title) and settings (type of questions and their specific settings)
+- to manage questions and answers (label settings, input aid, mandatory state, conditionals and options)
+- to exploit the results of each survey by exporting to CSV format
+
+ http://docs.rubedo-project.org/en/homepage/use/webmaster/surveys 
---
+++ config/module.config.php
@@ -0,0 +1,70 @@
+<?php
+return array(
+    'localisationfiles' => array(
+        'module/Survey/data/locale/languagekey/survey.json'
+    ),
+    'blocksDefinition' => array(
+        'form' => array(
+            'maxlifeTime' => -1,
+            'definitionFile' => realpath(__DIR__ . "/blocks/") . '/form.json'
+        )
+    ),
+    'controllers' => array(
+        'invokables' => array(
+            'Survey\\Backoffice\\Controller\\Forms' => 'Survey\\Backoffice\\Controller\\FormsController'
+        )
+    ),
+    'router' => array(
+        'routes' => array(
+            // Backoffice route : prefix by backoffice
+            'surveyBackOffice' => array(
+                'type' => 'Literal',
+                'options' => array(
+                    'route' => '/backoffice/forms',
+                    'defaults' => array(
+                        '__NAMESPACE__' => 'Survey\\Backoffice\\Controller',
+                        'controller' => 'forms',
+                        'action' => 'index'
+                    )
+                ),
+                'may_terminate' => true,
+                'child_routes' => array(
+                    'default' => array(
+                        'type' => 'Segment',
+                        'options' => array(
+                            'route' => '/[:action]',
+                            '__NAMESPACE__' => 'Survey\\Backoffice\\Controller',
+                            'constraints' => array(
+                                'controller' => 'forms',
+                                'action' => '[a-zA-Z][a-zA-Z0-9_-]*'
+                            ),
+                            'defaults' => array()
+                        )
+                    )
+                )
+            )
+        )
+    ),
+    'appExtension' => array(
+        'survey' => array(
+            'basePath' => realpath(__DIR__ . '/../app-extension') . '/acn',
+            'definitionFile' => realpath(__DIR__ . '/../app-extension') . '/acn.json'
+        )
+    ),
+    'service_manager' => array(
+        'invokables' => array(
+            'Forms' => 'Survey\\Service\\Forms',
+            'FormsResponses' => 'Survey\\Service\\FormsResponses'
+        )
+    ),
+    'namespaces_api' => array(
+        'Survey',
+    ),
+    'extension_paths' => array(
+        'survey' => array(
+            'path' => realpath(__DIR__ . '/../block/survey'),
+            'css' => array(),
+            'js' => array('js/survey.js'),
+        ),
+    ),
+);
